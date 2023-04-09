6f373fda6cb2f8a22cf6f32f114abb23294768d0
mojo <igormalj@gmail.com>
2023-04-09 21:56:56 +0200

    Add selectionSort algo

--- src/Algorithms/Sorting/SelectionSort/selectionSort.ts
+++ src/Algorithms/Sorting/SelectionSort/selectionSort.ts
@@ -1,7 +1,28 @@
 type SortInput = number[] | string[];
 
 function selectionSort(arr: SortInput): SortInput {
-  return arr;
+  const copy = [...arr] as SortInput;
+  let smallestValue;
+  let smallestIndex;
+
+  for (let i = 0; i < copy.length; i += 1) {
+    smallestValue = copy[i];
+    smallestIndex = i;
+
+    for (let j = i; j < copy.length; j += 1) {
+      if (copy[j] < smallestValue) {
+        smallestValue = copy[j];
+        smallestIndex = j;
+      }
+    }
+
+    if (smallestIndex !== i) {
+      const temp = copy[i];
+      copy[i] = copy[smallestIndex];
+      copy[smallestIndex] = temp;
+    }
+  }
+  return copy;
 }
 
 export default selectionSort;
---
+++ src/notes.ts
@@ -0,0 +1,22 @@
+// Good code
+// - readable
+// - scalable (Big O)
+// measure runtime with performance.now() => milliseconds
+// Big O notation is the language we use for talking about how long algorithm
+// takes to run. We can compare it to diff algos or functions using big O and
+// say which one is better then thr other when it comes to scale (regardless
+// of our computer differences).
+// Example: as elements in array increases => how many more operations do we
+// have to do. <= algorithmic efficiency.
+// Big O allows us to explain this concept, when we grow bigger and bigger with
+// our input, how much does the algo slow down. The less slow down(slower slows
+// down) the better it is.
+//
+// > O(n)
+// Linear (time) rate as our number of input increases, the number of operations
+// increases as well.
+// n means the big O depends on the number of inputs
+// [1,2,3,4] => O(4)
+// Big O doesn't measure things in seconds => instead we're focusing on how
+// quickly our runtime grows => doing this by using the size of the input, which
+// we call n and compare it to the number of operations that increase.
