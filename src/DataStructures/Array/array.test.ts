6051eddd5f74634242005de32019d9b9021a34f4
mojo <igormalj@gmail.com>
2023-04-15 21:45:21 +0200

    Add custom array methods

--- src/DataStructures/Array/array.test.ts
+++ src/DataStructures/Array/array.test.ts
@@ -44,6 +44,72 @@ describe('Custom Array Data Structure', () => {
     expect(lastItem).toBe('b');
   });
 
+  test('add item at the beginning of array', () => {
+    const arr = new ArrayDS<string>();
+    arr.push('a');
+    arr.push('b');
+    arr.unshift('c');
+    arr.unshift('d');
+
+    expect(arr).toEqual({
+      _length: 4,
+      _data: { 0: 'd', 1: 'c', 2: 'a', 3: 'b' },
+    });
+  });
+
+  test('remove item from the beginning of array', () => {
+    const arr = new ArrayDS<string>();
+    arr.push('a');
+    arr.push('b');
+    arr.unshift('c');
+    arr.unshift('d');
+    const firstItem = arr.shift();
+    arr.shift();
+    expect(firstItem).toBe('d');
+    expect(arr).toEqual({
+      _length: 2,
+      _data: { 0: 'a', 1: 'b' },
+    });
+  });
+
+  describe('insert item on given index into array', () => {
+    test('invalid index', () => {
+      const arr = new ArrayDS<string>();
+      arr.insert(-1, 'a');
+      arr.insert(1, 'b');
+      expect(arr).toEqual({ _length: 0, _data: {} });
+    });
+
+    test('insert at the start', () => {
+      const arr = new ArrayDS<string>();
+      arr.insert(1, 'No!');
+      arr.insert(0, 'a');
+      expect(arr).toEqual({ _length: 1, _data: { 0: 'a' } });
+    });
+
+    test('insert at the end', () => {
+      const arr = new ArrayDS<string>();
+      arr.push('mid');
+      arr.insert(1, 'end');
+      arr.insert(0, 'start');
+      expect(arr).toEqual({
+        _length: 3,
+        _data: { 0: 'start', 1: 'mid', 2: 'end' },
+      });
+    });
+
+    test('insert in to the middle', () => {
+      const arr = new ArrayDS<string>();
+      arr.push('start');
+      arr.push('end');
+      arr.insert(1, 'mid');
+      expect(arr).toEqual({
+        _length: 3,
+        _data: { 0: 'start', 1: 'mid', 2: 'end' },
+      });
+    });
+  });
+
   describe('delete item on given index from array', () => {
     test('delete from the start', () => {
       const arr = new ArrayDS<string>();
@@ -93,15 +159,48 @@ describe('Custom Array Data Structure', () => {
       expect(result).toBe('abc');
     });
 
+    test('map over array and filter out values in new array', () => {
+      const arr = new ArrayDS<string>();
+      arr.push('a');
+      arr.push('B');
+      arr.push('c');
+      const upperCase = arr.filter(
+        (str: string): boolean => str === str.toUpperCase()
+      );
+      const lowerCase = arr.filter(
+        (str: string): boolean => str === str.toLowerCase()
+      );
+      expect(upperCase).toEqual({
+        _length: 1,
+        _data: { 0: 'B' },
+      });
+      expect(lowerCase).toEqual({
+        _length: 2,
+        _data: { 0: 'a', 1: 'c' },
+      });
+      // original arr should not be modified
+      expect(arr).toEqual({
+        _length: 3,
+        _data: { 0: 'a', 1: 'B', 2: 'c' },
+      });
+    });
+
     test('map over array and return transformed value', () => {
       const arr = new ArrayDS<string>();
       arr.push('a');
       arr.push('b');
       arr.push('c');
-      const transformedArr = arr.map((str: string) => `-${str}-`);
+      const transformedArr = arr.map(
+        (str: string, i: number) => `${i + 1}.${str}`
+      );
       expect(transformedArr).toEqual({
         _length: 3,
-        _data: { 0: '-a-', 1: '-b-', 2: '-c-' },
+        _data: { 0: '1.a', 1: '2.b', 2: '3.c' },
+      });
+      // original arr should not be modified
+      expect(arr).toEqual({
+        _length: 3,
+        _data: { 0: 'a', 1: 'b', 2: 'c' },
       });
     });
 
@@ -111,10 +210,94 @@ describe('Custom Array Data Structure', () => {
       arr.push('b');
       arr.push('c');
       const reducedValue = arr.reduce(
-        (acc: string, ele: string) => acc + ele,
-        ''
+        (acc: string, ele: string) => `${acc + ele}`,
+        '=>'
       );
-      expect(reducedValue).toBe('abc');
+      expect(reducedValue).toBe('=>abc');
     });
+
+    test('reduce array to single value with numbers', () => {
+      const arr = new ArrayDS<number>();
+      arr.push(1);
+      arr.push(5);
+      arr.push(3);
+      const reducedValue = arr.reduce(
+        (acc: number, ele: number) => acc + ele,
+        10
+      );
+      expect(reducedValue).toBe(19);
+    });
+
+    test('return boolean if array contains item', () => {
+      const arr = new ArrayDS<string>();
+      arr.push('a');
+      arr.push('b');
+      arr.push('c');
+      expect(arr.any((str: string): boolean => str === 'a')).toBeTruthy();
+      expect(arr.any((str: string): boolean => str === 'd')).toBeFalsy();
+    });
+
+    test('return boolean if all items pass conditional callback', () => {
+      const arr = new ArrayDS<string>();
+      arr.push('a');
+      arr.push('b');
+      arr.push('c');
+      expect(
+        arr.every((str: string): boolean => typeof str === 'string')
+      ).toBeTruthy();
+      expect(arr.every((srt: string): boolean => srt > 'b')).toBeFalsy();
+    });
+
+    test('return first item that pass conditional callback', () => {
+      const arr = new ArrayDS<string>();
+      arr.push('a');
+      arr.push('b');
+      arr.push('c');
+      expect(arr.find((str: string): boolean => str === 'c')).toBe('c');
+      expect(arr.find((str: string): boolean => str === 'x')).toBe(undefined);
+    });
+
+    test('return first index of item that pass conditional callback', () => {
+      const arr = new ArrayDS<string>();
+      arr.push('a');
+      arr.push('b');
+      arr.push('c');
+      expect(arr.indexOf((str: string): boolean => str === 'c')).toBe(2);
+      expect(arr.indexOf((str: string): boolean => str === 'x')).toBe(
+        undefined
+      );
+    });
+  });
+
+  test('revers array', () => {
+    const arr = new ArrayDS<string>();
+    arr.push('a');
+    arr.push('b');
+    arr.push('c');
+    const reversed = arr.reverse();
+    expect(arr).toEqual({
+      _length: 3,
+      _data: { 0: 'a', 1: 'b', 2: 'c' },
+    });
+    expect(reversed).toEqual({
+      _length: 3,
+      _data: { 0: 'c', 1: 'b', 2: 'a' },
+    });
+  });
+
+  test('join array into string', () => {
+    const arr = new ArrayDS<number>();
+    arr.push(1);
+    arr.push(2);
+    arr.push(3);
+    const joined = arr.join(' + ');
+    expect(joined).toBe('1 + 2 + 3');
+
+    const arr2 = new ArrayDS<{ name: string }>();
+    arr2.push({ name: 'Foo' });
+    arr2.push({ name: 'Bar' });
+    arr2.push({ name: 'Baz' });
+    const joined2 = arr2.join(' + ');
+    expect(joined2).toBe('[object Object] + [object Object] + [object Object]');
   });
 });
--- src/DataStructures/Array/array.ts
+++ src/DataStructures/Array/array.ts
@@ -1,31 +1,36 @@
 /* eslint-disable no-underscore-dangle */
 
-type IteratorType<T> = {
-  (): (ele: T, index?: number, obj?: object) => void;
-};
-
 interface ArrayDSInterface<T> {
   _length: number;
   _data: {
     [key: number]: T;
   };
   get(index: number): void;
-  push(item: T): void;
+  push(item: T): number;
   pop(): T | undefined;
-  // unshift(item: T): void;
-  // shift(): T | undefined;
+  unshift(item: T): number;
+  shift(): T | undefined;
   delete(index: number): T | undefined;
-  // reverse
-  forEach(callback: (ele: T, index?: number, obj?: object) => void): void;
-  // filter
-  // map
-  // reduce
-  // any
-  // every
-  // find
-  // indexOf
+  insert(index: number, item: T): number | undefined;
+  forEach(callback: (ele: T, index: number, obj: object) => void): void;
+  filter(
+    callback: (ele: T, index: number, obj: object) => boolean
+  ): ArrayDSInterface<T>;
+  map(callback: (ele: T, index: number, obj: object) => T): ArrayDSInterface<T>;
+  reduce(
+    callback: (accumulator: T, ele: T, index: number, obj: object) => T,
+    initialValue: T
+  ): T;
+  any(callback: (ele: T, index: number, obj: object) => boolean): boolean;
+  every(callback: (ele: T, index: number, obj: object) => boolean): boolean;
+  find(
+    callback: (ele: T, index: number, obj: object) => boolean
+  ): T | undefined;
+  indexOf(callback: (ele: T) => boolean): number | undefined;
+  reverse(): ArrayDSInterface<T>;
+  join(separator: string): string;
+  slice(start: number, end: number): ArrayDSInterface<T>;
   // sort
-  // join
 }
 
 class ArrayDS<T> implements ArrayDSInterface<T> {
@@ -55,6 +60,13 @@ class ArrayDS<T> implements ArrayDSInterface<T> {
     this.deleteLastItem();
   }
 
+  private unshiftItems(index: number, item: T) {
+    for (let i = this._length; i >= index; i -= 1) {
+      this._data[i] = i === index ? item : this._data[i - 1];
+    }
+    this._length += 1;
+  }
+
   // public methods
   get(index: number) {
     return this._data[index];
@@ -73,25 +85,123 @@ class ArrayDS<T> implements ArrayDSInterface<T> {
     return lastItem;
   }
 
-  delete(index: number) {
+  unshift(item: T) {
+    this.unshiftItems(0, item);
+    return this.length;
+  }
+
+  shift(): T | undefined {
+    if (this._length === 0) return;
+    const firstItem = this._data[0];
+    this.shiftItems(0);
+    return firstItem;
+  }
+
+  delete(index: number): T | undefined {
     if (!this._data[index]) return;
     const deletedItem = this._data[index];
     this.shiftItems(index);
     return deletedItem;
   }
 
-  forEach(callBack: (ele: T, index?: number, obj?: object) => void): void {
+  insert(index: number, item: T): number | undefined {
+    if (index < 0 || index > this._length) return;
+    this.unshiftItems(index, item);
+    return this._length;
+  }
+
+  forEach(callback: (ele: T, index: number, obj: object) => void): void {
     for (let i = 0; i < this._length; i += 1) {
-      callBack(this._data[i], i, this);
+      callback(this._data[i], i, this);
     }
   }
 
-  filter(callBack: (ele: T, index?: number, obj?: object):boolean => void): T[] {
-    const filtered: T[] = [];
-    for(let i = 0; i < this._length; i += 1) {
-
+  filter(
+    callback: (ele: T, index: number, obj: object) => boolean
+  ): ArrayDS<T> {
+    const filtered = new ArrayDS<T>();
+    for (let i = 0; i < this._length; i += 1) {
+      if (callback(this._data[i], i, this)) filtered.push(this._data[i]);
     }
     return filtered;
+  }
+
+  map(callback: (ele: T, index: number, obj: object) => T): ArrayDS<T> {
+    const transformed = new ArrayDS<T>();
+    for (let i = 0; i < this._length; i += 1) {
+      transformed.push(callback(this._data[i], i, this));
+    }
+    return transformed;
+  }
+
+  reduce(
+    callback: (accumulator: T, ele: T, index: number, obj: object) => T,
+    initialValue: T
+  ): T {
+    let result: T = initialValue;
+    for (let i = 0; i < this._length; i += 1) {
+      result = callback(result, this._data[i], i, this);
+    }
+
+    return result;
+  }
+
+  any(callback: (ele: T, index: number, obj: object) => boolean): boolean {
+    for (let i = 0; i < this._length; i += 1) {
+      if (callback(this._data[i], i, this)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  every(callback: (ele: T, index: number, obj: object) => boolean): boolean {
+    for (let i = 0; i < this._length; i += 1) {
+      if (!callback(this._data[i], i, this)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  find(
+    callback: (ele: T, index: number, obj: object) => boolean
+  ): T | undefined {
+    for (let i = 0; i < this._length; i += 1) {
+      if (callback(this._data[i], i, this)) return this._data[i];
+    }
+    return undefined;
+  }
+
+  indexOf(callback: (ele: T) => boolean): number | undefined {
+    for (let i = 0; i < this._length; i += 1) {
+      if (callback(this._data[i])) return i;
+    }
+    return undefined;
+  }
+
+  reverse(): ArrayDS<T> {
+    const reversed = new ArrayDS<T>();
+    for (let i = this.length - 1; i >= 0; i -= 1) {
+      reversed.push(this._data[i]);
+    }
+    return reversed;
+  }
+
+  join(separator: string): string {
+    let joined = '';
+    for (let i = 0; i < this._length; i += 1) {
+      joined +=
+        typeof this._data[i] === 'object'
+          ? '[object Object]'
+          : `${this._data[i]}`;
+      if (i < this._length - 1) joined += separator;
+    }
+    return joined;
+  }
+
+  slice(start: number, end: number): ArrayDS<T> {
+    return this;
   }
 }
 
