5bc1d2544421a9fede764ddd21305686c291b887
mojo2 <igormalj@gamil.com>
2023-05-06 19:08:38 +0200

    add quicksort algo

--- src/Algorithms/Sorting/QuickSort/quickSort.test.ts
+++ src/Algorithms/Sorting/QuickSort/quickSort.test.ts
@@ -3,45 +3,40 @@ import quickSort from './quickSort';
 
 describe('sort array using quick sort algorithm', () => {
   test('sort numbers', () => {
-    expect(quickSort([])).toEqual([]);
-    expect(quickSort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
-    expect(quickSort([2, 3, 1, 4, 5])).toEqual([1, 2, 3, 4, 5]);
-    expect(quickSort([2, 4, 1, 3, 5])).toEqual([1, 2, 3, 4, 5]);
-    expect(quickSort([5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5]);
-    expect(quickSort([10, 14, 28, 11, 7, 16, 30, 50, 25, 18])).toEqual([
-      7, 10, 11, 14, 16, 18, 25, 28, 30, 50,
-    ]);
+    const arr1: number[] = [];
+    const arr2 = [1, 2, 3, 4, 5];
+    const arr3 = [2, 3, 1, 4, 5];
+    const arr4 = [2, 4, 1, 3, 5];
+    const arr5 = [5, 4, 3, 2, 1];
+    const arr6 = [10, 14, 28, 11, 7, 16, 30, 50, 25, 18];
+    quickSort(arr1);
+    expect(arr1).toEqual([]);
+    quickSort(arr2);
+    expect(arr2).toEqual([1, 2, 3, 4, 5]);
+    quickSort(arr3);
+    expect(arr3).toEqual([1, 2, 3, 4, 5]);
+    quickSort(arr4);
+    expect(arr4).toEqual([1, 2, 3, 4, 5]);
+    quickSort(arr5);
+    expect(arr5).toEqual([1, 2, 3, 4, 5]);
+    quickSort(arr6);
+    expect(arr6).toEqual([7, 10, 11, 14, 16, 18, 25, 28, 30, 50]);
   });
 
   test('sort strings', () => {
-    expect(quickSort(['a', 'd', 'b', 'e', 'c'])).toEqual([
-      'a',
-      'b',
-      'c',
-      'd',
-      'e',
-    ]);
-    expect(quickSort(['E', 'B', 'C', 'A', 'D'])).toEqual([
-      'A',
-      'B',
-      'C',
-      'D',
-      'E',
-    ]);
-    expect(quickSort(['a', 'A', 'b', 'C', 'c'])).toEqual([
-      'A',
-      'C',
-      'a',
-      'b',
-      'c',
-    ]);
-    expect(quickSort(['This', 'is', 'a', 'Sorting', 'example'])).toEqual([
-      'Sorting',
-      'This',
-      'a',
-      'example',
-      'is',
-    ]);
+    const arr1 = ['a', 'd', 'b', 'e', 'c'];
+    const arr2 = ['E', 'B', 'C', 'A', 'D'];
+    const arr3 = ['a', 'A', 'b', 'C', 'c'];
+    const arr4 = ['This', 'is', 'a', 'Sorting', 'example'];
+
+    quickSort(arr1);
+    expect(arr1).toEqual(['a', 'b', 'c', 'd', 'e']);
+    quickSort(arr2);
+    expect(arr2).toEqual(['A', 'B', 'C', 'D', 'E']);
+    quickSort(arr3);
+    expect(arr3).toEqual(['A', 'C', 'a', 'b', 'c']);
+    quickSort(arr4);
+    expect(arr4).toEqual(['Sorting', 'This', 'a', 'example', 'is']);
   });
 
   test('sort with callback', () => {});
--- src/Algorithms/Sorting/QuickSort/quickSort.ts
+++ src/Algorithms/Sorting/QuickSort/quickSort.ts
@@ -1,11 +1,54 @@
-// pivot - one of the items in the array that meets following conditions after sort
-// 1. correct position in final, sorted arr
-// 2. items to the left are smaller
-// 3. items to the right are larger
-// how to choose pivot
+/* eslint-disable no-param-reassign */
+// select one of the element from the list (any element) => pivot element
+// rearrange list => lesser | pivot | greater
+// example
+// [7, 2, 1, 6, 8, 5, 3, 4]
+// [2, 1, 3]  [4]  [8, 5, 7, 6]
+// after partition two new sub-problems (lesset and grater) =>
+// divide and conquer recursive
+// base case =>
+// recursive case =>
+// to keep space complexity O(1) => sort in place, not making arr copyes
+// we use arr referece and swop elements in original array
 
-function pivot() {}
+// helpers
+const swap = <T>(arrRef: T[], startIndex: number, endIndex: number) => {
+  const temp = arrRef[startIndex];
+  arrRef[startIndex] = arrRef[endIndex];
+  arrRef[endIndex] = temp;
+};
+// partition function
+const partition = <T>(arrRef: T[], startIndex: number, endIndex: number) => {
+  const pivot = arrRef[endIndex];
+  let pivotIndex = startIndex;
+  // loop through array and swap elements if element <= pivot and increment pivot
+  for (let i = startIndex; i < endIndex; i += 1) {
+    if (arrRef[i] <= pivot) {
+      swap(arrRef, i, pivotIndex);
+      pivotIndex += 1;
+    }
+  }
+  // after loop array looks [elements|pivot] =>
+  // [lesser<pivotIndex></pivotIndex>greater|pivot] => swap pivot with element
+  // at pivot index element => [lesser|pivot|grater]
+  swap(arrRef, pivotIndex, endIndex);
+  return pivotIndex;
+};
+// quick sort recursive function
 
-function quickSort() {}
+const quickSortRecursion = <T>(
+  arrRef: T[],
+  startIndex: number,
+  endIndex: number
+) => {
+  if (startIndex > endIndex) return; // base case => array have one element = sorted
+  const pivotIndex = partition(arrRef, startIndex, endIndex);
+  quickSortRecursion(arrRef, 0, pivotIndex - 1);
+  quickSortRecursion(arrRef, pivotIndex + 1, endIndex);
+};
+
+const quickSort = <T>(arrRef: T[]) => {
+  quickSortRecursion(arrRef, 0, arrRef.length - 1);
+};
 
 export default quickSort;
