9afd389aeb9d961c104c5bafa7dcafd762c96ae8
mojo <igormalj@gmail.com>
2023-05-13 22:22:47 +0200

    add fib dynamic

--- src/DynamicProgramming/Fibonacci/fibonacci.test.ts
+++ src/DynamicProgramming/Fibonacci/fibonacci.test.ts
@@ -1,2 +1,60 @@
-import { describe,test, expect } from "vitest";
-import 
+import { describe, test, expect } from 'vitest';
+import { fibonacciWithCalls, fibonacciMemoized } from './fibonacci';
+
+describe('test memoized fibonacci', () => {
+  test('number 3', () => {
+    const { getCalls: callsNonMemo, fibonacci: fibNonMemo } =
+      fibonacciWithCalls();
+    const { getCalls, fibonacci } = fibonacciMemoized();
+
+    const nonMemoResult = fibNonMemo(3);
+    const memoResult = fibonacci(3);
+
+    expect(callsNonMemo()).toBe(5);
+    expect(getCalls()).toBe(5);
+    expect(nonMemoResult).toBe(2);
+    expect(memoResult).toBe(2);
+  });
+
+  test('number 10', () => {
+    const { getCalls: callsNonMemo, fibonacci: fibNonMemo } =
+      fibonacciWithCalls();
+    const { getCalls, fibonacci } = fibonacciMemoized();
+
+    const nonMemoResult = fibNonMemo(10);
+    const memoResult = fibonacci(10);
+
+    expect(callsNonMemo()).toBe(177);
+    expect(getCalls()).toBe(19);
+    expect(nonMemoResult).toBe(55);
+    expect(memoResult).toBe(55);
+  });
+
+  test('number 10', () => {
+    const { getCalls: callsNonMemo, fibonacci: fibNonMemo } =
+      fibonacciWithCalls();
+    const { getCalls, fibonacci } = fibonacciMemoized();
+
+    const nonMemoResult = fibNonMemo(15);
+    const memoResult = fibonacci(15);
+
+    expect(callsNonMemo()).toBe(1973);
+    expect(getCalls()).toBe(29);
+    expect(nonMemoResult).toBe(610);
+    expect(memoResult).toBe(610);
+  });
+
+  test('number 35', () => {
+    const { getCalls: callsNonMemo, fibonacci: fibNonMemo } =
+      fibonacciWithCalls();
+    const { getCalls, fibonacci } = fibonacciMemoized();
+
+    const nonMemoResult = fibNonMemo(35);
+    const memoResult = fibonacci(35);
+
+    expect(callsNonMemo()).toBe(29860703);
+    expect(getCalls()).toBe(69);
+    expect(nonMemoResult).toBe(9227465);
+    expect(memoResult).toBe(9227465);
+  });
+});
--- src/DynamicProgramming/Fibonacci/fibonacci.ts
+++ src/DynamicProgramming/Fibonacci/fibonacci.ts
@@ -1,12 +1,37 @@
-const fibonacci = (number: number) => {
-  if (number <= 1) return number;
-  return fibonacci(number - 1) + fibonacci(number - 2);
+const fibonacciWithCalls = () => {
+  let calls = 0;
+
+  const getCalls = () => calls;
+
+  const fibonacci = (number: number): number => {
+    calls += 1;
+    if (number <= 1) return number;
+    return fibonacci(number - 1) + fibonacci(number - 2);
+  };
+
+  return { fibonacci, getCalls };
 };
 
 const fibonacciMemoized = () => {
-  const cache = {};
+  const cache: { [key: number]: number } = {};
+  let calls = 0; // only for testing/learning
 
-  return (number: number) => {};
+  const getCalls = () => calls;
+
+  const fibonacci = (number: number): number => {
+    calls += 1;
+    if (cache[number]) return cache[number];
+
+    if (number <= 1) {
+      cache[number] = number;
+      return number;
+    }
+    const result = fibonacci(number - 1) + fibonacci(number - 2);
+    cache[number] = result;
+    return result;
+  };
+
+  return { fibonacci, getCalls };
 };
 
-export default fibonacciMemoized;
+export { fibonacciWithCalls, fibonacciMemoized };
