f7051f5adfcca2e6979cfa3989f420e6e8e27f6a
mojo <igormalj@gmail.com>
2023-05-24 23:01:30 +0200

    add insert node to heap

--- src/DataStructures/Heap/BinaryHeapArray/binaryHeapArray.test.ts
+++ src/DataStructures/Heap/BinaryHeapArray/binaryHeapArray.test.ts
@@ -3,7 +3,19 @@ import BinaryHeapArray from './binaryHeapArray';
 
 describe('Binary Heap with Array implementation', () => {
   test('insertion', () => {
-    const heap = BinaryHeapArray<string>(5);
+    const heap = BinaryHeapArray<string>('max', 11);
     expect(heap.print()).toBe('');
+    heap.insert(1, 'a');
+    heap.insert(3, 'b');
+    heap.insert(5, 'c');
+    heap.insert(4, 'd');
+    heap.insert(6, 'e');
+    heap.insert(13, 'f');
+    heap.insert(10, 'g');
+    heap.insert(9, 'h');
+    heap.insert(8, 'i');
+    heap.insert(15, 'j');
+    heap.insert(17, 'l');
+    console.log(heap.print());
   });
 });
--- src/DataStructures/Heap/BinaryHeapArray/binaryHeapArray.ts
+++ src/DataStructures/Heap/BinaryHeapArray/binaryHeapArray.ts
@@ -1,6 +1,10 @@
-type Node<T> = {
+type NodeType<T> = {
   id: number;
   value: T;
+};
+
+const Node = <T>(id: number, value: T): NodeType<T> => {
+  return { id, value };
 };
 
 type TempHeap<T> = {
@@ -8,14 +12,41 @@ type TempHeap<T> = {
   insert: (id: number, value: T) => void;
 };
 
-const BinaryHeapArray = <T>(size: number): TempHeap<T> => {
-  const heap: (Node<T> | null)[] = Array.from({ length: size }, () => null);
+const BinaryHeapArray = <T>(type: 'max' | 'min', size: number): TempHeap<T> => {
+  const maxLength = size;
+  const heap: (NodeType<T> | null)[] = [];
 
   const print = () => {
-    return heap.filter((node) => node?.id).join(' ');
+    return heap.map((node) => node?.id).join(' ');
   };
 
-  const insert = (id: number, value: T) => {};
+  // maintains heap property (max/min)
+  const maxHeapify = (arr: NodeType[], heapSize: number, index: number) => {};
+
+  const minHeapify = () => {};
+
+  const insert = (id: number, value: T) => {
+    if (heap.length === maxLength) return undefined;
+    const newNode = Node(id, value);
+    heap.push(newNode);
+    let index = heap.length - 1;
+
+    while (index !== 0) {
+      console.log(index);
+      const parent = Math.floor(index / 2);
+      if (heap[index]!.id >= heap[parent]!.id) {
+        const temp = heap[index];
+        heap[index] = heap[parent];
+        heap[parent] = temp;
+        console.log('x');
+        index = parent;
+      } else {
+        index = 0;
+      }
+    }
+
+    console.log(newNode);
+  };
 
   return { print, insert };
 };
