f1e6a22b98ef09619b94c11cfe41039276687742
mojotron <igormalj@gmail.com>
2023-05-30 09:48:14 +0200

    add insert method and helpers

--- src/DataStructures/PriorityQueue/PriorityQueueHeap/priorityQueueHeap.test.ts
+++ src/DataStructures/PriorityQueue/PriorityQueueHeap/priorityQueueHeap.test.ts
@@ -1,2 +1,52 @@
 import { describe, test, expect } from 'vitest';
 import PriorityQueueHeap from './priorityQueueHeap';
+
+describe('Priority Queue, Heap Implementation', () => {
+  test('isEmpty', () => {
+    const pq = PriorityQueueHeap<string>();
+    expect(pq.isEmpty()).toBe(true);
+    pq.insert(1, 'A');
+    expect(pq.isEmpty()).toBe(false);
+    pq.pull();
+    expect(pq.isEmpty()).toBe(true);
+  });
+
+  test('insert', () => {
+    const pq = PriorityQueueHeap<string>();
+    expect(pq.size).toBe(0);
+
+    pq.insert(4, 'A');
+    expect(pq.size).toBe(1);
+    expect(pq.peek()).toBe('A');
+
+    pq.insert(5, 'B');
+    expect(pq.size).toBe(2);
+    expect(pq.peek()).toBe('A');
+
+    pq.insert(3, 'C');
+    expect(pq.size).toBe(3);
+    expect(pq.peek()).toBe('C');
+
+    pq.insert(3, 'D');
+    expect(pq.size).toBe(4);
+    expect(pq.peek()).toBe('C');
+
+    pq.insert(3, 'E');
+    expect(pq.size).toBe(5);
+    expect(pq.peek()).toBe('C');
+
+    pq.insert(2, 'F');
+    expect(pq.size).toBe(6);
+    expect(pq.peek()).toBe('F');
+
+    pq.insert(6, 'G');
+    expect(pq.size).toBe(7);
+    expect(pq.peek()).toBe('F');
+
+    pq.insert(1, 'H');
+    expect(pq.size).toBe(8);
+    expect(pq.peek()).toBe('H');
+  });
+
+  test('pull minimum value out', () => {});
+});
--- src/DataStructures/PriorityQueue/PriorityQueueHeap/priorityQueueHeap.ts
+++ src/DataStructures/PriorityQueue/PriorityQueueHeap/priorityQueueHeap.ts
@@ -1,29 +1,52 @@
 import { PriorityQueueType, NodeArrayType } from '../types';
 
 const Node = <T>(priority: number, value: T): NodeArrayType<T> => {
-  return {priority, value}
-}
+  return { priority, value };
+};
 
 // Heap           | enqueue() | dequeue() | peek()
 // Time Complexity|  O(lon n) |  O(log n) |  O(1)
 
 // min priority implementation
-const PriorityQueueHeap = <T>(): PriorityQueueType<T> => {
+const PriorityQueueHeap = <T>(): PriorityQueueType<T> & {
+  stringify: () => string;
+} => {
   const queue: NodeArrayType<T>[] = []; // heap
 
   const isEmpty = (): boolean => {
     return queue.length === 0;
   };
 
-  const bubbleUp = () => {};
+  const bubbleUp = (index: number) => {
+    let position = index;
+    while (position > 0) {
+      const parentIndex = Math.floor((position - 1) / 2);
+      if (queue[position].priority < queue[parentIndex].priority) {
+        const swap = queue[position];
+        queue[position] = queue[parentIndex];
+        queue[parentIndex] = swap;
+        position = parentIndex;
+      } else {
+        break;
+      }
+    }
+  };
 
-  const insert = (priority: number, value: T): void => {};
+  const insert = (priority: number, value: T): void => {
+    const newNode = Node(priority, value);
+    queue.push(newNode);
+    bubbleUp(queue.length - 1);
+  };
 
   const bubbleDown = () => {};
 
-  const pull = ():undefined | T => {};
+  const pull = (): undefined | T => {
+    if (isEmpty()) return undefined;
+    const last = queue.pop();
+    return last?.value;
+  };
 
-  const peek = ():undefined | T => {
+  const peek = (): undefined | T => {
     if (isEmpty()) return undefined;
     const topPriority = queue[0].value;
     if (typeof topPriority === 'object' && Array.isArray(topPriority))
@@ -33,13 +56,26 @@ const PriorityQueueHeap = <T>(): PriorityQueueType<T> => {
     return topPriority;
   };
 
+  const stringify = (): string => {
+    const result: string[] = [];
+    queue.forEach((ele) => {
+      result.push(`[${ele.priority}|${ele.value}]`);
+    });
+
+    return result.join(' ');
+  };
+
   return {
-    get size() {return queue.length}
+    get size() {
+      return queue.length;
+    },
     isEmpty,
     insert,
     pull,
     peek,
-  }
+    //
+    stringify,
+  };
 };
 
 export default PriorityQueueHeap;
